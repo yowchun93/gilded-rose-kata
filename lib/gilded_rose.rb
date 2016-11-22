42a0923b1e66c3752dc0f8557d792fec14d026ed
yowchun93 <yowchun93@gmail.com>
2016-11-22 17:40:34 +0800

    add tick method for backstage

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -65,6 +65,8 @@ class GildedRose
       return brie_tick
     when 'Sulfuras, Hand of Ragnaros'
       return sulfuras_tick
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      return backstage_tick
     end
   end
 
@@ -73,14 +75,22 @@ class GildedRose
   end
 
   def backstage_tick
-    
+    @days_remaining -= 1
+    return if @quality >= 50 
+    ## quality is 0 after sale date
+    return @quality = 0 if @days_remaining < 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10 
+    @quality += 1 if @days_remaining < 5 
+
   end
 
   def brie_tick
     @days_remaining -= 1
     return if @quality >= 50
     @quality += 1
-    @quality += 1 if @days_remaining <= 0 && @quality <= 50
+    @quality += 1 if @days_remaining <= 0 && @quality < 50
   end
 
   def normal_tick
